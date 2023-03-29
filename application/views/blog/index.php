296570f77368291b51e547cedc7a751c06cdfb02
Bed <obed.ouano@proweaver.net>
2023-03-29 18:09:00 +0800

    032923 - updated layout

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -3,12 +3,14 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800"><?php echo $title; ?></h1>
+                        <h1 class="h3 mb-0 text-gray-800"><?= $title; ?></h1>
       
                     </div>
 
                     <div class="row">
+                        <div class="col-sm-5">
                             <p>content here...</p>
+                        </div>
                     </div>
 
             </div>
